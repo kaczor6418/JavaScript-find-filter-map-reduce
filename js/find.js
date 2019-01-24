e5fb8fa151f9b00758050b2a870f0241ffb6a260
kaczor6418 <34687242+kaczor6418@users.noreply.github.com>
2019-01-24 22:41:17 +0100

    Update find.js

--- js/find.js
+++ js/find.js
@@ -1,4 +1,4 @@
-Array.prototype.myFind = function(callbackFunction = false) {
+Array.prototype.myFind = function(callbackFunction) {
 
     for (let i = 0; i < this.length; i++) {
         if (callbackFunction(this[i])) {
