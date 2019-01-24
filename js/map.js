f4f48c4c934667d1d3dc07a506e7e9e6e977e9a2
kaczor6418 <34687242+kaczor6418@users.noreply.github.com>
2019-01-24 22:43:20 +0100

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,4 +1,4 @@
-Array.prototype.myMap = function(callbackFunction = false) {
+Array.prototype.myMap = function(callbackFunction) {
     const mapped = [];
     for (let i = 0; i < this.length; i++) {
         let result = callbackFunction(this[i], i, this);
